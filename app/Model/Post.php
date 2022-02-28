03923f7a8858d52e712e861a88eb754857238a6e
Maccio92 <valerio362@gmail.com>
2022-02-28 15:43:19 +0100

    add seeder

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,10 +1,17 @@
 <?php
 
-namespace App;
+namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'author',
+        'content',
+        'slug',
+        'created_at',
+        'updated_at'
+    ];
 }
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Model\Post;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +12,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+        $post = new Post();
+        $post->title = $faker->sentence(3, true);
+        $post->author = $faker->words(2, true);
+        $post->content = $faker->text();
+        $post->slug = Str::slug($post->title . '-' . $i, '-');
+        $post->save();
+        }
     }
 }
