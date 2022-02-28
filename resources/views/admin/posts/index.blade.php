f72583f234698c6f499ed5c71e5c85a51ad18fdb
Maccio92 <valerio362@gmail.com>
2022-02-28 16:49:00 +0100

    add show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,20 +1,23 @@
-<!-- @dd($posts) -->
 @extends ('layouts.app')
 
 @section('content')
-<h2 class="p-2 bg-secondary">Posts:</h2>
+<div class="d-flex align-items-center justify-content-around bg-secondary">
+    <h2 class="p-2 ">Posts:</h2>
+    <a class="btn btn-primary" href="{{ route('admin.posts.create') }}">Add Post</a>
+</div>
+
 <div class="container">
-    <div class="row">
+    <div class="row flex-column">
         @foreach($posts as $post)
-    <div class="d-flex my-3 align-items-center justify-content-between border-bottom border-1">
-        <div class="my-1">
-            <h3 class="m-0 text-capitalize">{{$post -> title}}</h3>
-        </div>
-        <div class="d-flex gap-5">
-            <button class="btn btn-primary"><a class="text-light" href="{{ route('admin.posts.create', $post) }}">Vai</a></button>
-        </div>
-    </div>
-    @endforeach
+            <div class="d-flex my-3 align-items-center justify-content-between border-bottom border-1">
+                <div class="my-1">
+                    <h3 class="m-0 text-capitalize">{{$post -> title}}</h3>
+                </div>
+                <div class="d-flex gap-5">
+                    <button class="btn btn-primary"><a class="text-light" href="{{ route('admin.posts.show', $post) }}">Vai</a></button>
+                </div>
+            </div>
+        @endforeach
     {{ $posts->links()}}
     </div>    
     
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row bg-dark p-3">
+            <div class="col">
+                <h1 class="text-light text-capitalize">{{ $post->title }}</h1>
+            </div>
+        </div>
+        <div class="row border-bottom border-2 p-3">
+            <div class="col-12">
+                <h2 class="text-dark text-capitalize">autore: {{ $post->author }}</h2>
+            </div>
+        </div>
+        <div class="row border-bottom border-2 p-3 bg-info">
+            <div class="col">
+                <p class="text-dark">Descrizione:<br>{{ $post->content }}</p>
+            </div>
+        </div>
+        <div class="p-3">
+            <button class="btn btn-primary"><a class="text-light text-capitalize p-2" href="{{ route('admin.posts.index') }}">Back</a></button>
+        </div>
+    </div>
+@endsection
